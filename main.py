a95ac717fdbdc96fe25e850743992b444931692c
grzesiu <grzspychala@gmail.com>
2018-04-24 12:35:29 +0200

    Update argument parsing

--- main.py
+++ main.py
@@ -9,18 +9,19 @@ from sklearn.preprocessing import LabelBinarizer
 import util
 from structures.document import Document
 
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--train_dir', default='Train/DrugBank/', help='Train directory')
-    parser.add_argument('--test_dir', default='Test/Test for DrugNER task/DrugBank', help='Test directory')
+    parser.add_argument('--train', type=str, help='Train directory')
+    parser.add_argument('--test', type=str, help='Test directory')
+    parser.add_argument('--tune', help='Should tune the parameters', action='store_true')
+    parser.add_argument('--save', help='Should save the output', action='store_true')
     return parser.parse_args()
 
-def read_docs(directory):
-    train_docs = [Document.from_file(os.path.join(directory, filename))
-                  for filename in os.listdir(directory)]
-    return train_docs
 
-def extract_data(docs):
+def extract_data(directory):
+    docs = [Document.from_file(os.path.join(directory, filename))
+            for filename in os.listdir(directory)]
     sentences_as_iobs = [sentence.to_iobs() for doc in docs for sentence in doc.sentences]
     x = [util.sentence_to_features(sentence_as_iobs) for sentence_as_iobs in sentences_as_iobs]
     y = [util.sentence_to_labels(sentence_as_iobs) for sentence_as_iobs in sentences_as_iobs]
@@ -73,22 +74,34 @@ def evaluate(x_test, y_test):
     print(bio_classification_report(y_test, y_pred))
 
 
+def tune(x_train, y_train, x_test, y_test):
+    for c2 in [1e-3, 1e-2, 1e-1, 1, 10, 100]:
+        for possible_transitions in [True, False]:
+            print("c1=" + str(0) + ", c2=" + str(c2) + ", trans=" + str(possible_transitions))
+            train(x_train, y_train, c1=0, c2=c2, max_iterations=1000, possible_transitions=possible_transitions)
+            evaluate(x_test, y_test)
+
+
 def main():
     args = parse_args()
-    train_docs = read_docs(args.train_dir)
-    test_docs = read_docs(args.test_dir)
-    x_train, y_train = extract_data(train_docs)
-    x_test, y_test = extract_data(test_docs)
-    # for c2 in [1e-3, 1e-2, 1e-1, 1, 10, 100]:
-    #     for possible_transitions in [True, False]:
-    #         print("c1="+str(0)+ ", c2="+str(c2)+ ", trans="+str(possible_transitions))
-    #         train(x_train, y_train, c1=0, c2=c2, max_iterations=1000, possible_transitions=possible_transitions)
-    #         evaluate(x_test, y_test)
-    #S    
-    # Train with the best parameters found so far
-    train(x_train, y_train, c1=0.001, c2=1, max_iterations=1000, possible_transitions=True)
-    evaluate(x_test, y_test)
+    x_train, y_train, x_test, y_test = None, None, None, None
 
+    if args.train:
+        x_train, y_train = extract_data(args.train)
+        train(x_train, y_train, c1=0.001, c2=1, max_iterations=1000, possible_transitions=True)
+
+    if args.test and not args.tune:
+        x_test, y_test = extract_data(args.test)
+        evaluate(x_test, y_test)
+
+        if args.save:
+            pass
+
+    elif args.tune and args.train and args.test:
+        tune(x_train, y_train, x_test, y_test)
+
+        if args.save:
+            pass
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -1,16 +1,12 @@
 from nltk.stem import PorterStemmer
 from nltk.stem import WordNetLemmatizer
 
-import pandas as pd
-import re
-from re import IGNORECASE
-
-
-with open('.\DrugBank.csv', encoding="utf8") as f:
+with open('DrugBank.csv', encoding="utf8") as f:
     names = f.read().splitlines()
-names = set(names)    
+names = set(names)
 
-def common_tokens(text, type, n=10): #Maybe treat these as separte feature and try if each of them improves the model?
+
+def common_tokens(text, type, n=10):  # Maybe treat these as separte feature and try if each of them improves the model?
     '''
         Check if the word contains any of the most common 2-gram and 3-gram, as done in 
         "UMCC_DLSI: Semantic and Lexical features for detection and classification Drugs in biomedical texts"
@@ -19,16 +15,17 @@ def common_tokens(text, type, n=10): #Maybe treat these as separte feature and t
             ::type: whether it is the word itself (iob), the preceeding one (iob_p) or the following one (iob_n)
             ::n: the number of n-grams to consider (max = 10). The 10 most common n-grams are extracted from the paper and hardly encoded
     '''
-    if n > 10: 
+    if n > 10:
         n = 10
     ngrams = ["in", "ne", "ine", "ti", "id", "an", "ro", "nt", "et", "en"]
     features = []
     for i in range(n):
         ng = ngrams[i]
         c = text.count(ng)
-        features.append(type+"."+ng+"="+str(c)) # Not sure if the algo is able to understand that this is a number
-    return features 
-    
+        features.append(
+            type + "." + ng + "=" + str(c))  # Not sure if the algo is able to understand that this is a number
+    return features
+
 
 def issingular(text):
     ''' Return if a word is singular or plural - note, should be better evaluated to see if it works well with drugs 
@@ -38,57 +35,59 @@ def issingular(text):
     lemma = wnl.lemmatize(text, 'n')
     return False if text is not lemma else True
 
+
 def isInDrugDictionary(text):
     ''' Return if a drug is present in the WordBank dictionary (common names and synonyms are considered)
         Also, partial match should be implemented
     '''
-    #r =  re.compile('.*'+text+'.*')
+    # r =  re.compile('.*'+text+'.*')
     if text in names:
         return "Match"
-    #elif  any(r.match(line) for line in names):
+    # elif  any(r.match(line) for line in names):
     #    return "Match"
-    else: 
+    else:
         return "NoMatch"
+
 
 def iob_to_features(iob_index, sentence_as_iobs):
     iob = sentence_as_iobs[iob_index]
-    
+
     ps = PorterStemmer()
 
     features = [
         'bias',
         'iob.lower=' + iob.text.lower(),
-        'word.stem=' + ps.stem(iob.text), # Used by Greg (=a) #
-        'iob[:3]=' + iob.text[:3], # Greg (-a) #
-        'iob[:2]=' + iob.text[:2], # Greg (-a) #
-        'iob[-3:]=' + iob.text[-3:], 
+        'word.stem=' + ps.stem(iob.text),  # Used by Greg (=a) #
+        'iob[:3]=' + iob.text[:3],  # Greg (-a) #
+        'iob[:2]=' + iob.text[:2],  # Greg (-a) #
+        'iob[-3:]=' + iob.text[-3:],
         'iob[-2:]=' + iob.text[-2:],
         'iob.isupper=%s' % iob.text.isupper(),
         'iob.istitle=%s' % iob.text.istitle(),
         'iob.isdigit=%s' % iob.text.isdigit(),
-        'iob.singular=%s' % issingular(iob.text), #
-        'iob.isInDictionary=%s' % isInDrugDictionary(iob.text), #(=a but diff numbers)
+        'iob.singular=%s' % issingular(iob.text),  #
+        'iob.isInDictionary=%s' % isInDrugDictionary(iob.text),  # (=a but diff numbers)
         'pos_tag=' + iob.pos_label,
         'pos_tag[:2]=' + iob.pos_label[:2],
     ]
-    features.extend(common_tokens(iob.text, "iob", n=5)) #5
+    features.extend(common_tokens(iob.text, "iob", n=5))  # 5
     if iob_index > 0:
         iob_p = sentence_as_iobs[iob_index - 1]
         features.extend([
             '-1:iob.lower=' + iob_p.text.lower(),
-            '-1:iob.stem=' + ps.stem(iob_p.text), # (-a) #
-            '-1:iob[-3:]=' + iob_p.text[-3:], #(=a, !=c) #
-            '-1:iob[-2:]=' + iob_p.text[-2:], #(-a) #
-            '-1:iob[:3]=' + iob_p.text[:3], # Greg (-a) #
-            '-1:iob[:2]=' + iob_p.text[:2], # Greg (-a) #
+            '-1:iob.stem=' + ps.stem(iob_p.text),  # (-a) #
+            '-1:iob[-3:]=' + iob_p.text[-3:],  # (=a, !=c) #
+            '-1:iob[-2:]=' + iob_p.text[-2:],  # (-a) #
+            '-1:iob[:3]=' + iob_p.text[:3],  # Greg (-a) #
+            '-1:iob[:2]=' + iob_p.text[:2],  # Greg (-a) #
             '-1:iob.istitle=%s' % iob_p.text.istitle(),
             '-1:iob.isupper=%s' % iob_p.text.isupper(),
-            '-1:iob.singular=%s' % issingular(iob_p.text), #
-            '-1:iob.isInDictionary=%s' % isInDrugDictionary(iob_p.text), #(-a) #
+            '-1:iob.singular=%s' % issingular(iob_p.text),  #
+            '-1:iob.isInDictionary=%s' % isInDrugDictionary(iob_p.text),  # (-a) #
             '-1:pos_tag=' + iob_p.pos_label,
             '-1:pos_tag[:2]=' + iob_p.pos_label[:2],
         ])
-        features.extend(common_tokens(iob_p.text, "-1:iob", n=10)) #(-a) #
+        features.extend(common_tokens(iob_p.text, "-1:iob", n=10))  # (-a) #
     else:
         features.append('BOS')
 
@@ -96,19 +95,19 @@ def iob_to_features(iob_index, sentence_as_iobs):
         iob_n = sentence_as_iobs[iob_index + 1]
         features.extend([
             '+1:iob.lower=' + iob_n.text.lower(),
-            '+1:iob.stem=' + ps.stem(iob_n.text), # (-a)  #
-            '+1:iob[-3:]=' + iob_n.text[-3:], #(-a) #
-            '+1:iob[-2:]=' + iob_n.text[-2:], #(+a) !
-            '+1:iob[:3]=' + iob_n.text[:3], # Greg (-a) #
-            '+1:iob[:2]=' + iob_n.text[:2], # Greg (-a) #
+            '+1:iob.stem=' + ps.stem(iob_n.text),  # (-a)  #
+            '+1:iob[-3:]=' + iob_n.text[-3:],  # (-a) #
+            '+1:iob[-2:]=' + iob_n.text[-2:],  # (+a) !
+            '+1:iob[:3]=' + iob_n.text[:3],  # Greg (-a) #
+            '+1:iob[:2]=' + iob_n.text[:2],  # Greg (-a) #
             '+1:iob.istitle=%s' % iob_n.text.istitle(),
             '+1:iob.isupper=%s' % iob_n.text.isupper(),
-            '+1:iob.singular=%s' % issingular(iob_n.text), #
-            '+1:iob.isInDictionary=%s' % isInDrugDictionary(iob_n.text), 
+            '+1:iob.singular=%s' % issingular(iob_n.text),  #
+            '+1:iob.isInDictionary=%s' % isInDrugDictionary(iob_n.text),
             '+1:pos_tag=' + iob_n.pos_label,
             '+1:pos_tag[:2]=' + iob_n.pos_label[:2],
         ])
-        features.extend(common_tokens(iob_n.text, "+1:iob", n=10)) #(-a) #
+        features.extend(common_tokens(iob_n.text, "+1:iob", n=10))  # (-a) #
     else:
         features.append('EOS')
 
